a41925208a4cca705f753773e73631ce02fc2815
AlenYegar <202130481401@mail.scut.edu.cn>
2024-06-11 11:34:30 +0800

    完成各个内容视图的大致框架，添加对应路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import DashboardPage from '@/views/DashboardPage.vue'
 import LoginPage from '@/views/LoginPage.vue'
 import RegisterPage from '@/views/RegisterPage.vue'
+import PartManagement from '@/views/ContentView/PartManagement.vue'
+import BomManagement from '@/views/ContentView/BomManagement.vue'
+import ClassficationManagement from '@/views/ContentView/ClassficationManagement.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +33,30 @@ const router = createRouter({
       component: RegisterPage,
       meta: {
         title: '注册'
+      }
+    },
+    {
+      path: '/partManagement',
+      name: 'PartManagement',
+      component: PartManagement,
+      meta: {
+        title: '部件管理'
+      }
+    },
+    {
+      path: '/bomManagement',
+      name: 'BomManagement',
+      component: BomManagement,
+      meta: {
+        title: 'BOM管理'
+      }
+    },
+    {
+      path: '/classficationManagement',
+      name: 'ClassficationManagement',
+      component: ClassficationManagement,
+      meta: {
+        title: '分类管理'
       }
     }
   ]
--- src/views/ContentView.vue/BomManagement.vue
+++ src/views/ContentView.vue/BomManagement.vue
@@ -1,9 +1,23 @@
 <template>
-
+    <div class="part-management">
+        <el-container>
+            <el-header>
+                <TheBreadcrumb />
+            </el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <!-- 导航菜单 -->
+                    <TheNavMenu />
+                </el-aside>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script setup>
-
+import TheBreadcrumb from '@/components/TheBreadcrumb.vue'
+import TheNavMenu from '@/components/TheNavMenu.vue'
 </script>
 
 <style lang="scss">
